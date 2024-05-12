cc6eb110e9ab4903430d1d34d88627dd1503f09e
7Rena7 <renacorbe@gmail.com>
2024-05-11 21:12:54 -0300

    Refactorizar estrategia martingala a la clase Martingala que hereda de Estrategia.
    Refactorizar estrategia DAlembert para que tome los valores que vienen de la clase Estrategia en vez de hardocodearlos en el archivo (CAPITAL y APUESTA_INICIAL). Formatear el archivo  con el formateador de VSCode.

--- tp1-2-analisis-economico-ruleta/dalambert.py
+++ tp1-2-analisis-economico-ruleta/dalambert.py
@@ -3,31 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from estrategia import Estrategia
 
- 
-CAPITAL = 10000
-APUESTA_INICIAL = 100
 
 class Dalambert(Estrategia):
     def __init__(self, cant_tiradas, cant_corridas, numero_elegido, tipo_capital='i' or 'f'):
         super().__init__(cant_tiradas, cant_corridas, numero_elegido, tipo_capital)
 
-        # self.listado_apuestas = []
-        # self.listado_capital = []
-        # self.listado_unidades = []
-        # self.listado_wins = []
         self.es_capital_infinito = True if tipo_capital == 'i' else False
-        
-        self.capital = 0
-        
-        self.apuesta_inicial = APUESTA_INICIAL
-        self.rojo = [1, 3, 5, 7, 9, 12, 14, 16,
-                     18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
-    
-    def estrategia(self,is_win):
-    # def estrategia(self,listado_apuestas,is_win):
+        self.capital = self.capital_inicial
+
+    def estrategia(self, is_win):
         """
         Implementa un sistema de apuestas basado en la estrategia de D'alambert.
-        
+
         Si la apuesta es ganadora, entonces se dismunye la cantidad apostada en una unidad
         SI la apuesta es perdedora, se aumenta la cantidad apostada en una unidad
 
@@ -39,12 +26,12 @@ class Dalambert(Estrategia):
         int: El monto de la próxima apuesta según la estrategia Fibonacci.
         """
 
-        # global capital, apuesta_inicial
         ultima_apuesta = self.listado_apuestas[-1]
-        unidad = self.apuesta_inicial 
+        unidad = self.apuesta_inicial
         if is_win:
             self.capital += ultima_apuesta
-            proxima_apuesta = max(ultima_apuesta - unidad, unidad)  # Evitar apuestas menores que la unidad inicial
+            # Evitar apuestas menores que la unidad inicial
+            proxima_apuesta = max(ultima_apuesta - unidad, unidad)
         else:
             self.capital -= ultima_apuesta
             proxima_apuesta = ultima_apuesta + unidad
@@ -59,81 +46,69 @@ class Dalambert(Estrategia):
         self.listado_apuestas.append(self.apuesta_inicial)
         self.listado_capital.append(self.capital)
         self.listado_unidades.append(1)
-        self.capital = CAPITAL
+        self.capital = self.capital_inicial
 
-    def grafico_flujo_caja(self,listados_capital):
-        fig, ax1 = plt.subplots(figsize=(20, 10))
+    def grafico_flujo_caja(self, listados_capital):
+        plt.subplots(figsize=(10, 5))
 
-        # Configuración del primer eje Y para el capital
         color = 'tab:blue'
-        ax1.set_xlabel('Número de Tiradas')
-        # ax1.set_ylabel('Capital Total', color=color)
-        ax1.set_ylabel('Capital Total')
+        plt.xlabel('Número de Tiradas')
+        plt.ylabel('Capital Total')
 
-        for corrida, listado_capital in enumerate(listados_capital):
-            print(corrida,listado_capital)
-            ax1.plot(listado_capital, label=f'Corrida {corrida+1}', alpha=0.5)
+        for _, listado_capital in enumerate(listados_capital):
+            plt.plot(listado_capital, alpha=0.75)
 
-        ax1.axhline(y=CAPITAL, color='r', linestyle='-', label='Capital total')
-        ax1.tick_params(axis='y', labelcolor=color)
-        ax1.grid(True)
+        plt.axhline(y=self.capital_inicial, color='r',
+                    linestyle='-', label='Capital total')
+        plt.tick_params(axis='y', labelcolor=color)
+        plt.grid(True)
 
-        # Configuración de ticks del eje X cada 10 tiradas
-        # max_len = max(len(listado) for listado in listados_capital)
-        # ticks = np.arange(0, max_len, int(max_len/10))
-        # ax1.set_xticks(ticks)
-        # ax1.set_xticklabels(ticks)
-
-        ax1.legend()
+        plt.legend()
         plt.show()
-
-
 
     def generar_graficos(self, data_simulacion):
         # listados_capital = [df['capital'].tolist() for df in data_simulacion]
         listados_capital = [df['capital'] for df in data_simulacion]
         self.grafico_flujo_caja(listados_capital)
 
-
     def ejecutar_estrategia(self):
         data_simulacion = []
-        for i in range(0,self.cant_corridas):
-            print("Iteracion",i)
+        for _ in range(0, self.cant_corridas):
             data_simulacion.append(self.simulacion())
 
         self.generar_graficos(data_simulacion)
 
-    
     def simulacion(self):
 
         self.inicializar_valores()
 
         tirada = 0
-        while True:  
+        while True:
             tirada += 1
-            numero_al_girar_ruleta = np.random.randint(0,37)
-            is_win = numero_al_girar_ruleta in self.rojo
+            numero_al_girar_ruleta = np.random.randint(0, 37)
+            is_win = numero_al_girar_ruleta in self.resultados_rojo
 
             self.listado_wins.append(is_win)
             proxima_apuesta = self.estrategia(is_win)
-            
-            if not self.es_capital_infinito and proxima_apuesta > self.capital: # banca rota
+
+            if not self.es_capital_infinito and proxima_apuesta > self.capital:  # banca rota
                 break
 
             if self.es_capital_infinito and len(self.listado_apuestas) == self.cant_tiradas:
                 break
 
-            self.listado_unidades.append(self.listado_unidades[-1] +1 if is_win else self.listado_unidades[-1] -1 )
+            self.listado_unidades.append(
+                self.listado_unidades[-1] + 1 if is_win else self.listado_unidades[-1] - 1)
             # listado_unidades.append( sum(listado_unidades) +1 if is_win else sum(listado_unidades) -1 )
             self.listado_apuestas.append(proxima_apuesta)
             self.listado_capital.append(self.capital)
 
         return pd.DataFrame({
-                'apuesta': self.listado_apuestas,
-                'win': self.listado_wins,
-                'capital': self.listado_capital,
-                'balance': self.listado_unidades
-                        })
+            'apuesta': self.listado_apuestas,
+            'win': self.listado_wins,
+            'capital': self.listado_capital,
+            'balance': self.listado_unidades})
+
 
 if __name__ == "__main__":
     # Definimos los parámetros
@@ -143,42 +118,8 @@ if __name__ == "__main__":
     tipo_capital = 'f'  # 'i' para capital infinito, 'f' para capital fijo
 
     # Creamos la instancia
-    estrategia = Dalambert(cant_tiradas, cant_corridas, numero_elegido, tipo_capital)
+    estrategia = Dalambert(cant_tiradas, cant_corridas,
+                           numero_elegido, tipo_capital)
 
     # Ejecutamos la estrategia
     estrategia.ejecutar_estrategia()
-
-
-
-        # def grafico_flujo_caja(listado_capital):    
-        
-    #     fig, ax1 = plt.subplots(figsize=(20, 10))
-
-    #     # Configuración del primer eje Y para el capital
-    #     color = 'tab:blue'
-    #     ax1.set_xlabel('Número de Tiradas')
-    #     ax1.set_ylabel('Capital Total', color=color)
-    #     ax1.plot(listado_capital, label='Capital Total', color=color)
-    #     ax1.axhline(y=CAPITAL, color='r', linestyle='-', label='Capital total')
-
-    #     # Ganancia maxima
-    #     max_value =max(listado_capital)
-    #     max_index = listado_capital.index(max_value)
-    #     ax1.axvline(x=max_index, color='b', linestyle='--', label='Ganancia máxima')
-    #     ax1.axhline(y=max_value, color='b', linestyle='--')
-    #     ax1.scatter([max_index], [max_value], color='b', s=100)
-    #     ax1.annotate(f'({max_value:.2f}, {max_index})', 
-    #                 (max_index, max_value), 
-    #                 textcoords="offset points", 
-    #                 xytext=(50,10), 
-    #                 color="b",
-    #                 ha='center')
-
-    #     # ax1.ax()
-    #     ax1.tick_params(axis='y', labelcolor=color)
-    #     ax1.grid(True)
-
-    #     # Configuración de ticks del eje X cada 10 tiradas
-    #     ticks = np.arange(0, len(listado_capital),int(len(listado_capital)/10))
-    #     ax1.set_xticks(ticks)
-    #     ax1.set_xticklabels(ticks)
--- tp1-2-analisis-economico-ruleta/martingala.py
+++ tp1-2-analisis-economico-ruleta/martingala.py
@@ -5,96 +5,166 @@ from estrategia import Estrategia
 
 
 class Martingala(Estrategia):
-    def __init__(self, cant_tiradas, cant_corridas, numero_elegido, tipo_capital='i' or 'f',):
+
+    def __init__(self, cant_tiradas, cant_corridas, numero_elegido, tipo_capital='i' or 'f'):
         super().__init__(cant_tiradas, cant_corridas, numero_elegido, tipo_capital)
+        self.tiradas = np.random.randint(0, 37, size=[self.cant_tiradas])
+        self.listado_apuestas = []
+        self.listado_capital = []
+        self.listado_wins = []
+        self.capital = self.capital_inicial
 
     def ejecutar_estrategia(self):
-        print('Ejecutando estrategia de Martingala...')
+        self.listado_apuestas.append(self.apuesta_inicial)
+        self.listado_capital.append(self.capital)
 
+        for tirada in self.tiradas:
+            is_win = tirada in self.resultados_rojo
+            self.listado_wins.append(is_win)
 
-cantidad_tiradas = 10000
-apuesta_inicial = 1
-capital = 100
-tipo_capital = 'f'
-rojo = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+            proxima_apuesta = self.martingala(
+                self.listado_apuestas[-1], is_win)
 
+            if proxima_apuesta > self.capital and self.tipo_capital == 'f':  # banca rota
+                break
 
-def martingala(apuesta_actual, is_win):
-    """
-    Implementa la estrategia de apuestas de Martingala.
-    Si la apuesta es ganadora, se vuelve a apostar la apuesta inicial.
-    Si la apuesta es perdedora, se duplica la apuesta.
+            if len(self.listado_apuestas) == self.cant_tiradas:
+                break
 
-    Parameters:
-    apuesta_actual (float): La cantidad apostada en la última tirada.
-    is_win (bool): Indica si la última apuesta fue ganadora (True) o perdedora (False).
+            self.listado_apuestas.append(proxima_apuesta)
+            self.listado_capital.append(self.capital)
 
-    Returns:
-    float: El monto de la próxima apuesta según la estrategia de Martingala.
-    """
-    global capital, apuesta_inicial
+        # Crear DataFrame
+        df = pd.DataFrame({
+            'apuesta': self.listado_apuestas,
+            'win': self.listado_wins,
+            'capital': self.listado_capital
+        })
 
-    if is_win:
-        capital += apuesta_actual
-        proxima_apuesta = apuesta_inicial
-    else:
-        capital -= apuesta_actual
-        proxima_apuesta = apuesta_actual * 2
+        # Mostrar gráfico
+        self.grafico_flujo_caja()
 
-    return proxima_apuesta
+    def martingala(self, apuesta_actual, is_win):
+        """
+        Implementa la estrategia de apuestas de Martingala.
+        Si la apuesta es ganadora, se vuelve a apostar la apuesta inicial.
+        Si la apuesta es perdedora, se duplica la apuesta.
 
+        Parameters:
+        apuesta_actual (float): La cantidad apostada en la última tirada.
+        is_win (bool): Indica si la última apuesta fue ganadora (True) o perdedora (False).
 
-tiradas = np.random.randint(0, 37, size=[cantidad_tiradas])
-listado_apuestas = []
-listado_capital = []
-listado_wins = []
+        Returns:
+        float: El monto de la próxima apuesta según la estrategia de Martingala.
+        """
+        if is_win:
+            self.capital += apuesta_actual
+            proxima_apuesta = self.apuesta_inicial
+        else:
+            self.capital -= apuesta_actual
+            proxima_apuesta = apuesta_actual * 2
 
-listado_apuestas.append(apuesta_inicial)
-listado_capital.append(capital)
+        return proxima_apuesta
 
-for tirada in tiradas:
-    is_win = tirada in rojo
-    listado_wins.append(is_win)
+    def grafico_flujo_caja(self):
+        global cantidad_tiradas
 
-    proxima_apuesta = martingala(listado_apuestas[-1], is_win)
+        _, ax = plt.subplots()
 
-    if proxima_apuesta > capital and tipo_capital == 'f':  # banca rota
-        break
+        ax.set_title(
+            f"ESTRATEGIA MARTINGALA - CAPITAL INICIAL: {self.listado_capital[0]}")
+        ax.set_xlabel('n (Número de tiradas)')
+        ax.set_ylabel('cc (Cantidad de capital)')
 
-    if len(listado_apuestas) == cantidad_tiradas:
-        break
+        ax.plot([i for i in range(1, len(self.listado_apuestas) + 1)],
+                self.listado_capital, linewidth=2.0, label='fc (Flujo de caja)')
+        ax.axhline(self.listado_capital[0], color='r',
+                   linestyle='--', label='fci (Flujo de caja inicial)')
 
-    listado_apuestas.append(proxima_apuesta)
-    listado_capital.append(capital)
+        plt.legend()
+        plt.show()
 
-# Crear DataFrame
-df = pd.DataFrame({
-    'apuesta': listado_apuestas,
-    'win': listado_wins,
-    'capital': listado_capital
-})
+# Como estaba antes de la refactorización a clase Martingala 👇🏽👇🏽👇🏽
+# if __name__ == "__main__":
+#     cantidad_tiradas = 10000
+#     apuesta_inicial = 1
+#     capital = 100
+#     tipo_capital = 'f'
+#     rojo = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 
-# Función para graficar el flujo de caja
+#     def martingala(apuesta_actual, is_win):
+#         """
+#         Implementa la estrategia de apuestas de Martingala.
+#         Si la apuesta es ganadora, se vuelve a apostar la apuesta inicial.
+#         Si la apuesta es perdedora, se duplica la apuesta.
 
+#         Parameters:
+#         apuesta_actual (float): La cantidad apostada en la última tirada.
+#         is_win (bool): Indica si la última apuesta fue ganadora (True) o perdedora (False).
 
-def grafico_flujo_caja(listado_capital):
-    global cantidad_tiradas
+#         Returns:
+#         float: El monto de la próxima apuesta según la estrategia de Martingala.
+#         """
+#         global capital, apuesta_inicial
 
-    fig, ax = plt.subplots()
+#         if is_win:
+#             capital += apuesta_actual
+#             proxima_apuesta = apuesta_inicial
+#         else:
+#             capital -= apuesta_actual
+#             proxima_apuesta = apuesta_actual * 2
 
-    ax.set_title(
-        f"ESTRATEGIA MARTINGALA - CAPITAL INICIAL: {listado_capital[0]}")
-    ax.set_xlabel('n (Número de tiradas)')
-    ax.set_ylabel('cc (Cantidad de capital)')
+#         return proxima_apuesta
 
-    ax.plot([i for i in range(1, len(listado_apuestas) + 1)],
-            listado_capital, linewidth=2.0, label='fc (Flujo de caja)')
-    ax.axhline(listado_capital[0], color='r',
-               linestyle='--', label='fci (Flujo de caja inicial)')
+#     tiradas = np.random.randint(0, 37, size=[cantidad_tiradas])
+#     listado_apuestas = []
+#     listado_capital = []
+#     listado_wins = []
 
-    plt.legend()
-    plt.show()
+#     listado_apuestas.append(apuesta_inicial)
+#     listado_capital.append(capital)
 
+#     for tirada in tiradas:
+#         is_win = tirada in rojo
+#         listado_wins.append(is_win)
 
-# Mostrar gráfico
-grafico_flujo_caja(listado_capital)
+#         proxima_apuesta = martingala(listado_apuestas[-1], is_win)
+
+#         if proxima_apuesta > capital and tipo_capital == 'f':  # banca rota
+#             break
+
+#         if len(listado_apuestas) == cantidad_tiradas:
+#             break
+
+#         listado_apuestas.append(proxima_apuesta)
+#         listado_capital.append(capital)
+
+#     # Crear DataFrame
+#     df = pd.DataFrame({
+#         'apuesta': listado_apuestas,
+#         'win': listado_wins,
+#         'capital': listado_capital
+#     })
+
+#     # Función para graficar el flujo de caja
+
+#     def grafico_flujo_caja(listado_capital):
+#         global cantidad_tiradas
+
+#         fig, ax = plt.subplots()
+
+#         ax.set_title(
+#             f"ESTRATEGIA MARTINGALA - CAPITAL INICIAL: {listado_capital[0]}")
+#         ax.set_xlabel('n (Número de tiradas)')
+#         ax.set_ylabel('cc (Cantidad de capital)')
+
+#         ax.plot([i for i in range(1, len(listado_apuestas) + 1)],
+#                 listado_capital, linewidth=2.0, label='fc (Flujo de caja)')
+#         ax.axhline(listado_capital[0], color='r',
+#                    linestyle='--', label='fci (Flujo de caja inicial)')
+
+#         plt.legend()
+#         plt.show()
+
+#     # Mostrar gráfico
+#     grafico_flujo_caja(listado_capital)
