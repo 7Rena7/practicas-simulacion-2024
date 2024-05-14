cd9920c18e9f95d43673e71a811cd5d302632910
Mateo Rindello <mateorindello14@gmail.com>
2024-05-14 00:06:14 -0300

    Update paroli.py

--- tp1-2-analisis-economico-ruleta/paroli.py
+++ tp1-2-analisis-economico-ruleta/paroli.py
@@ -8,10 +8,11 @@ from estrategia import Estrategia
 class Paroli(Estrategia):
     def __init__(self, cant_tiradas, cant_corridas, numero_elegido, tipo_capital='i' or 'f', ):
         super().__init__(cant_tiradas, cant_corridas, numero_elegido, tipo_capital)
-        self.tiradas = np.random.randint(0, 37, size=[self.cant_tiradas])
-        self.listado_apuestas = []
-        self.listado_capital = []
-        self.listado_wins = []
+        self.listado_apuestas_total = []
+        self.listado_capital_total = []
+        self.listado_wins_total = []
+        self.listado_frecuencia_total = []
+        self.listado_dfs = []
         self.capital = self.capital_inicial
         self.victorias_seguidas = 0
 
@@ -25,7 +26,7 @@ class Paroli(Estrategia):
         Si la apuesta es perdedora, se apuesta lo mínimo.
 
         Parameters:
-        apuesta_actual (float): La cantidad apostada en la última tirada.
+        ultima_apuesta (float): La cantidad apostada en la última tirada.
         is_win (bool): Indica si la última apuesta fue ganadora (True) o perdedora (False).
 
         Returns:
@@ -48,53 +49,83 @@ class Paroli(Estrategia):
         return proxima_apuesta
 
     def grafico_flujo_caja(self):
-        global cantidad_tiradas
+        fig, ax = plt.subplots()
 
-        _, ax = plt.subplots()
+        ax.set_title(f"ESTRATEGIA PAROLI CAPITAL {self.tipo_capital}")
+        ax.set_xlabel('n (número de tiradas)')
+        ax.set_ylabel('c (capital)')
 
-        ax.set_title(
-            f"ESTRATEGIA PAROLI - CAPITAL INICIAL: {self.listado_capital[0]}")
-        ax.set_xlabel('n (Número de tiradas)')
-        ax.set_ylabel('cc (Cantidad de capital)')
+        for listado_capital in self.listado_capital_total:
+            ax.plot(listado_capital, linewidth=2.0, )
 
-        ax.plot([i for i in range(1, len(self.listado_apuestas) + 1)],
-                self.listado_capital, linewidth=2.0, label='fc (Flujo de caja)')
-        ax.axhline(self.listado_capital[0], color='r',
-                   linestyle='--', label='fci (Flujo de caja inicial)')
+        ax.axhline(self.capital_inicial, color='r', linestyle='--', label='fci (flujo de caja inicial)')
+
+        ax.ticklabel_format(axis='y', style='plain')
 
         plt.legend()
         plt.show()
 
+    def graficar_histograma(self):
+        fig, ax = plt.subplots()
+
+        ax.set_title(f"HISTOGRAMA {self.cant_corridas} CORRIDAS")
+        ax.set_xlabel('n (número de tiradas)')
+        ax.set_ylabel('fr (frecuencia relativa)')
+
+        for df in self.listado_dfs:
+            ax.bar(df.index, df['fr'], alpha=0.5)
+
+        plt.show()
+
     def ejecutar_estrategia(self):
-        self.listado_apuestas.append(self.apuesta_inicial)
-        self.listado_capital.append(self.capital)
+        print('Ejecutando estrategia de Paroli...')
 
-        for tirada in self.tiradas:
-            """
-            El sistema de Paroli no indica qué se debe apostar, si no cuanto según la cantidad de victorias seguidas.
-            Por lo tanto, como ejemplo para hacer una prueba, apuesto siempre al rojo.
-            """
-            is_win = tirada in self.resultados_rojo
-            self.listado_wins.append(is_win)
+        for corrida in range(self.cant_corridas):
 
-            proxima_apuesta = self.paroli(
-                self.listado_apuestas[-1], is_win)
+            self.capital = self.capital_inicial
+            tiradas = np.random.randint(0, 37, size=[self.cant_tiradas])
 
-            if proxima_apuesta > self.capital and self.tipo_capital == 'f':  # banca rota
-                break
+            listado_apuestas = []
+            listado_capital = []
+            listado_wins = []
+            listado_frecuencia_relativa = []
 
-            if len(self.listado_apuestas) == self.cant_tiradas:
-                break
+            listado_apuestas.append(self.apuesta_inicial)
+            listado_capital.append(self.capital)
 
-            self.listado_apuestas.append(proxima_apuesta)
-            self.listado_capital.append(self.capital)
+            for index, tirada in enumerate(tiradas):
 
-        # Crear DataFrame
-        df = pd.DataFrame({
-            'apuesta': self.listado_apuestas,
-            'win': self.listado_wins,
-            'capital': self.listado_capital
-        })
+                is_win = tirada in self.resultados_rojo
+                listado_wins.append(is_win)
+
+                listado_frecuencia_relativa.append(listado_wins.count(True) / (index + 1))
+
+                proxima_apuesta = self.paroli(listado_apuestas[-1], is_win)
+
+                if proxima_apuesta > self.capital and self.tipo_capital == 'f':  # banca rota
+                    break
+
+                if len(listado_apuestas) == self.cant_tiradas:
+                    break
+
+                listado_apuestas.append(proxima_apuesta)
+                listado_capital.append(self.capital)
+
+            self.listado_apuestas_total.append(listado_apuestas)
+            self.listado_capital_total.append(listado_capital)
+            self.listado_wins_total.append(listado_wins)
+            self.listado_frecuencia_total.append(listado_frecuencia_relativa)
+
+            df = pd.DataFrame({
+                'capital': listado_capital,
+                'apuesta': listado_apuestas,
+                'win': listado_wins,
+                'fr': listado_frecuencia_relativa
+            })
+
+            self.listado_dfs.append(df)
 
         # Mostrar gráfico
         self.grafico_flujo_caja()
+        self.graficar_histograma()
+
