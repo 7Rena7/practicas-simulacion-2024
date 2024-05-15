499755fb1c2b0de32eebe914406f18686cd3fbd4
Mateo Rindello <mateorindello14@gmail.com>
2024-05-15 16:05:45 -0300

    Agregar graficas promedio a estrategia Paroli

--- tp1-2-analisis-economico-ruleta/paroli.py
+++ tp1-2-analisis-economico-ruleta/paroli.py
@@ -57,6 +57,8 @@ class Paroli(Estrategia):
 
         for listado_capital in self.listado_capital_total:
             ax.plot(listado_capital, linewidth=2.0, )
+            max_value = max(listado_capital)
+            ax.scatter([0], [max_value])
 
         ax.axhline(self.capital_inicial, color='r', linestyle='--', label='fci (flujo de caja inicial)')
 
@@ -75,6 +77,56 @@ class Paroli(Estrategia):
         for df in self.listado_dfs:
             ax.bar(df.index, df['fr'], alpha=0.5)
 
+        plt.show()
+
+    def grafico_flujo_caja_promedio(self, listados_capital):
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        # Calcular el promedio del capital para cada tirada
+
+        max_tiradas = max(len(listado) for listado in listados_capital)  # Obtener la longitud máxima de las listas
+        promedio_capital = []
+        # promedio_capital = np.zeros(max_len)
+        for j in range(0, max_tiradas):
+            sum_capital_en_tirada_i = 0
+            for i in range(0, len(listados_capital)):
+                try:
+                    sum_capital_en_tirada_i += listados_capital[i][j]
+                except:
+                    sum_capital_en_tirada_i += 0
+            promedio_capital.append(sum_capital_en_tirada_i / self.cant_corridas)
+
+        # Graficar el promedio del capital
+        ax.plot(promedio_capital, label='Promedio del Capital', color='blue')
+        ax.set_title('Flujo de caja promedio a lo largo de las tiradas')
+        ax.set_xlabel('Número de Tiradas')
+        ax.set_ylabel('Capital Promedio')
+        ax.legend()
+        ax.grid(True)
+        plt.show()
+
+    def generar_histograma_promedio(self, listado_frecuencia_relativa):
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        max_tiradas = max(len(listado) for listado in listado_frecuencia_relativa)
+        promedio_frecuencias = []
+
+        for j in range(max_tiradas):
+            sum_frecuencias_en_tirada_j = 0
+            for i in range(len(listado_frecuencia_relativa)):
+                try:
+                    sum_frecuencias_en_tirada_j += listado_frecuencia_relativa[i][j]
+                except:
+                    sum_frecuencias_en_tirada_j += 0
+            promedio_frecuencias.append(sum_frecuencias_en_tirada_j / self.cant_corridas)
+
+        # Use a proper range for the x values
+        x_values = range(len(promedio_frecuencias))
+
+        ax.set_title(f"HISTOGRAMA PROMEDIO DE {self.cant_corridas} CORRIDAS")
+        ax.set_xlabel('n (número de tiradas)')
+        ax.set_ylabel('fr (frecuencia relativa)')
+        ax.bar(x_values, promedio_frecuencias, alpha=0.5)
         plt.show()
 
     def ejecutar_estrategia(self):
@@ -128,4 +180,10 @@ class Paroli(Estrategia):
         # Mostrar gráfico
         self.grafico_flujo_caja()
         self.graficar_histograma()
+
+        listados_capital = [df['capital'] for df in self.listado_dfs]
+        listado_frecuencias_relativas = [df["fr"] for df in self.listado_dfs]
+
+        self.grafico_flujo_caja_promedio(listados_capital)
+        self.generar_histograma_promedio(listado_frecuencias_relativas)
 
